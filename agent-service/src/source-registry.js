8ade83f22e7d4aadd561285ceb4c210a87181be6
Millham1 <145725492+Millham1@users.noreply.github.com>
2026-05-15 19:02:44 -0400

    Add trusted editorial source registry

---
+++ agent-service/src/source-registry.js
@@ -0,0 +1,45 @@
+module.exports = {
+  trustedCruiseSources: [
+    'Cruise Hive',
+    'Cruise Critic',
+    'Cruise Industry News',
+    'Norwegian Cruise Line',
+    'Royal Caribbean',
+    'Carnival Cruise Line',
+    'MSC Cruises'
+  ],
+
+  trustedMainstreamSources: [
+    'Reuters',
+    'Associated Press',
+    'AP News',
+    'CNN',
+    'CNN Travel',
+    'Fox News',
+    'Fox Weather',
+    'NBC News',
+    'ABC News',
+    'CBS News',
+    'USA Today',
+    'New York Times',
+    'Washington Post'
+  ],
+
+  trustedOperationalSources: [
+    'FAA',
+    'NOAA',
+    'National Hurricane Center',
+    'Weather.com'
+  ],
+
+  rejectedSourcePatterns: [
+    'tmz',
+    'daily mail',
+    'the sun',
+    'mirror',
+    'radar online',
+    'clickorlando',
+    'local12',
+    'patch.com'
+  ]
+};
